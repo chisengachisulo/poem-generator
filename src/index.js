fad562e99cb632f0383446b13a322f4a948203e0
chisengachisulo <128135390+chisengachisulo@users.noreply.github.com>
2024-01-26 10:37:51 +0000

    Added basic form handling and typewrite

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  let poemElement = document.querySelector("#poem");
+
+  new Typewriter("#poem", {
+    strings:
+      "A boat beneath a sunny sky, Lingering onward dreamily In an evening of july Eager eye and willing ear.",
+
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
