7bee072eced6928ff6cf500ace2b7c3596b02a19
chisengachisulo <128135390+chisengachisulo@users.noreply.github.com>
2024-01-26 11:09:32 +0000

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let poemElement = document.querySelector("#poem");
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings:
-      "A boat beneath a sunny sky, Lingering onward dreamily In an evening of july Eager eye and willing ear.",
-
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "ff3237836o6410aacb2481a69ct9c917";
+  let context =
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and seperate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'by AI with ❤️' inside a <strong></strong> element at the end of the poem and not the beginning";
+  let prompt = `User instructions: Generate a poem about${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
